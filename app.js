8f0915c03935f6da98790f5ac6df21ada8f30f01
Surya Kumara <suryakumara33@gmail.com>
2022-01-10 20:37:31 +0800

    ts-module

--- app.js
+++ app.js
@@ -1,2 +1,46 @@
 "use strict";
-console.log("Surya");
+const userName = "Surya";
+// let age = 10;
+// age = 20;
+function add(a, b = 1) {
+    let result;
+    result = a + b;
+    return result;
+}
+// perbedaan let dan variable adalah pada block scope menggunakan if statement
+// ini masih bekerja di javascript
+// if (age > 30) {
+//   var isOld = true;
+// }
+// console.log(isOld);
+// ini error di javascript
+// if (age > 30) {
+//   let isOld = true;
+// }
+// console.log(isOld);
+const printOut = (output) => console.log(output);
+const button = document.querySelector("button");
+if (button) {
+    button.addEventListener("click", (event) => console.log(event));
+}
+printOut(add(5));
+// spread operator
+const hobbies = ["Sports", "Cooking"];
+const activeHobbies = ["Hiking"];
+activeHobbies.push(...hobbies);
+const person = {
+    firstName: "Surya",
+    age: 25,
+};
+const copiedPerson = Object.assign({}, person);
+const substact = (...numbers) => {
+    return numbers.reduce((curResult, curValue) => {
+        return curResult + curValue;
+    }, 0);
+};
+const substractedNumbers = substact(2, 4, 56, 34);
+console.log(substractedNumbers);
+// destructuring
+const [hobby1, hobby2, ...remainingHobbies] = hobbies;
+const { firstName: userNames, age } = person;
+console.log(userName, age);
--- app.ts
+++ app.ts
@@ -1,1 +1,63 @@
-console.log("Surya");
+const userName = "Surya";
+
+// let age = 10;
+// age = 20;
+
+function add(a: number, b: number = 1) {
+  let result;
+  result = a + b;
+  return result;
+}
+
+// perbedaan let dan variable adalah pada block scope menggunakan if statement
+
+// ini masih bekerja di javascript
+// if (age > 30) {
+//   var isOld = true;
+// }
+// console.log(isOld);
+
+// ini error di javascript
+// if (age > 30) {
+//   let isOld = true;
+// }
+// console.log(isOld);
+
+const printOut: (a: number | string) => void = (output) => console.log(output);
+
+const button = document.querySelector("button");
+
+if (button) {
+  button.addEventListener("click", (event) => console.log(event));
+}
+
+printOut(add(5));
+
+// spread operator
+const hobbies = ["Sports", "Cooking"];
+const activeHobbies = ["Hiking"];
+activeHobbies.push(...hobbies);
+
+const person = {
+  firstName: "Surya",
+  age: 25,
+};
+
+const copiedPerson = {
+  ...person,
+};
+
+const substact = (...numbers: number[]) => {
+  return numbers.reduce((curResult, curValue) => {
+    return curResult + curValue;
+  }, 0);
+};
+
+const substractedNumbers = substact(2, 4, 56, 34);
+console.log(substractedNumbers);
+
+// destructuring
+
+const [hobby1, hobby2, ...remainingHobbies] = hobbies;
+const { firstName: userNames, age } = person;
+console.log(userName, age);
