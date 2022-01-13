9f2cf0b1b2761e1a41e8e66c4d399a6b5406f72f
Surya Kumara <suryakumara33@gmail.com>
2022-01-13 22:33:29 +0800

    inhiritance

--- app.js
+++ app.js
@@ -1,4 +1,19 @@
 "use strict";
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
 var Department = /** @class */ (function () {
     function Department(name, id) {
         this.name = name;
@@ -30,3 +45,35 @@ accounting.describe();
 accounting.addEmployees("Surya");
 accounting.addEmployees("Kumara");
 accounting.printEmployee();
+// inhiritent
+var ITDepartment = /** @class */ (function (_super) {
+    __extends(ITDepartment, _super);
+    function ITDepartment(id, admins) {
+        var _this = _super.call(this, "IT", id) || this;
+        _this.admins = admins;
+        _this.admins = admins;
+        return _this;
+    }
+    return ITDepartment;
+}(Department));
+var AccountingDepartment = /** @class */ (function (_super) {
+    __extends(AccountingDepartment, _super);
+    function AccountingDepartment(name, reports) {
+        var _this = _super.call(this, name, "ID") || this;
+        _this.reports = reports;
+        _this.reports = reports;
+        return _this;
+    }
+    AccountingDepartment.prototype.addReports = function (report) {
+        this.reports.push(report);
+    };
+    return AccountingDepartment;
+}(Department));
+var it = new ITDepartment("id3", ["Surya"]);
+it.addEmployees("Surya");
+it.addEmployees("Kumara");
+console.log(it);
+var acc = new AccountingDepartment("Surya", []);
+acc.addReports("Make a task");
+acc.addReports("Make a taskas");
+console.log(acc);
--- app.ts
+++ app.ts
@@ -2,7 +2,7 @@ class Department {
   // field of the class
   // private id: string;
   // private name: string;
-  private employees: string[] = [];
+  protected employees: string[] = [];
 
   constructor(public name: string, private readonly id: string) {
     // this.id = id;
@@ -33,3 +33,43 @@ accounting.describe();
 accounting.addEmployees("Surya");
 accounting.addEmployees("Kumara");
 accounting.printEmployee();
+
+// inhiritent
+class ITDepartment extends Department {
+  constructor(id: string, public admins: string[]) {
+    super("IT", id);
+    this.admins = admins;
+  }
+}
+
+class AccountingDepartment extends Department {
+  constructor(name: string, private reports: string[]) {
+    super(name, "ID");
+    this.reports = reports;
+  }
+
+  addReports(report: string) {
+    this.reports.push(report);
+  }
+
+  addEmployees(name: string): void {
+    if (name === "Surya") {
+      return;
+    }
+    this.employees.push(name);
+  }
+}
+
+const it = new ITDepartment("id3", ["Surya"]);
+it.addEmployees("Surya");
+it.addEmployees("Kumara");
+console.log(it);
+
+const acc = new AccountingDepartment("Surya", []);
+
+acc.addReports("Make a task");
+acc.addReports("Make a taskas");
+console.log(acc);
+
+// private property can't access to extend class
+//
