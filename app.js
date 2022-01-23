088e8d0f4a2aca75e03462512438fa420812e116
Surya Kumara <suryakumara33@gmail.com>
2022-01-23 09:24:01 +0800

    ts type casting

--- app.js
+++ app.js
@@ -1,21 +1,73 @@
 "use strict";
-var Person = /** @class */ (function () {
-    function Person(n) {
-        this.age = 30;
-        this.name = n;
+var e1 = {
+    name: "Max",
+    privileges: ["create-server"],
+    startDate: new Date(),
+};
+function tambah(a, b) {
+    if (typeof a === "string" || typeof b === "string") {
+        return a.toString() + b.toString();
     }
-    Person.prototype.greet = function (phrase) {
-        console.log(phrase + " " + this.name);
+    return a + b;
+}
+function printEmployeeInformation(emp) {
+    console.log("Name : " + emp.name);
+    if ("privileges" in emp) {
+        console.log("Privilages : " + emp.privileges);
+    }
+    if ("startDate" in emp) {
+        console.log("Privileges : " + emp.startDate);
+    }
+}
+printEmployeeInformation({ name: "Surya", startDate: new Date() });
+var Car = /** @class */ (function () {
+    function Car() {
+    }
+    Car.prototype.drive = function () {
+        console.log("Driving....");
     };
-    return Person;
+    return Car;
 }());
-var user1;
-user1 = new Person("I Made Surya Kumara");
-// user1 = {
-//   name: "I Made Surya Kumara",
-//   greet(phrase: string) {
-//     console.log(phrase + " " + this.name);
-//   },
-// };
-user1.greet("Hi there - I am ");
-console.log(user1);
+var Truck = /** @class */ (function () {
+    function Truck() {
+    }
+    Truck.prototype.drive = function () {
+        console.log("Driving...");
+    };
+    Truck.prototype.loadCargo = function (amount) {
+        console.log("Loading cargo ..." + amount);
+    };
+    return Truck;
+}());
+var v1 = new Car();
+var v2 = new Truck();
+function useVhecle(vehicles) {
+    vehicles.drive();
+    // if ("loadCargo" in vehicles) {
+    //   vehicles.loadCargo(1000);
+    // }
+    // atau bisa juga seperti ini
+    if (vehicles instanceof Truck) {
+        vehicles.loadCargo(1000);
+    }
+}
+useVhecle(v1);
+function moveAnimal(animal) {
+    var speed;
+    switch (animal.type) {
+        case "bird":
+            speed = animal.flyingSpeed;
+            break;
+        case "horse":
+            speed = animal.runningSpeed;
+        default:
+            break;
+    }
+    console.log(" Moving at speed: " + speed);
+}
+moveAnimal({ type: "bird", flyingSpeed: 10 });
+var paragraph = document.getElementById("message-output");
+console.log(paragraph);
+var userInput = document.getElementById("user-input"); // panggunaan ! untuk meyakinkan typescript bahwa ini tidak mungkin null
+userInput.value = "This is not true";
+console.log(userInput);
--- app.ts
+++ app.ts
@@ -1,52 +1,120 @@
-// type AddFn = (a: number, b: number) => number
-// interface function type
-interface AddFn {
-  (a: number, b: number): number;
-}
-
-let add: AddFn;
-add = (n1: number, n2: number) => {
-  return n1 + n2;
+type Admin = {
+  name: string;
+  privileges: string[];
 };
 
-interface Named {
-  readonly name?: string;
-  outputName?: string;
+type Employee = {
+  name: string;
+  startDate: Date;
+};
+
+type ElevatedEmployee = Admin & Employee;
+
+const e1: ElevatedEmployee = {
+  name: "Max",
+  privileges: ["create-server"],
+  startDate: new Date(),
+};
+
+// intersection type menggunakan &
+
+type Combinable = string | number;
+type Numeric = number | boolean;
+
+type Unversal = Combinable & Numeric;
+
+function tambah(a: Combinable, b: Combinable) {
+  if (typeof a === "string" || typeof b === "string") {
+    return a.toString() + b.toString();
+  }
+  return a + b;
 }
 
-interface Greetable extends Named {
-  greet(phrase: string): void;
-}
-class Person implements Greetable {
-  name?: string;
-  age = 30;
+type UnknownEmployee = Employee | Admin;
 
-  constructor(n?: string) {
-    if (n) {
-      this.name = n;
-    }
+function printEmployeeInformation(emp: UnknownEmployee) {
+  console.log("Name : " + emp.name);
+
+  if ("privileges" in emp) {
+    console.log("Privilages : " + emp.privileges);
   }
 
-  greet(phrase: string): void {
-    if (this.name) {
-      console.log(phrase + " " + this.name);
-    } else {
-      console.log("Hi !");
-    }
+  if ("startDate" in emp) {
+    console.log("Privileges : " + emp.startDate);
   }
 }
 
-let user1: Greetable;
+printEmployeeInformation({ name: "Surya", startDate: new Date() });
 
-user1 = new Person("I Made Surya Kumara");
-user1 = new Person();
+class Car {
+  drive() {
+    console.log("Driving....");
+  }
+}
 
-// user1 = {
-//   name: "I Made Surya Kumara",
-//   greet(phrase: string) {
-//     console.log(phrase + " " + this.name);
-//   },
-// };
+class Truck {
+  drive() {
+    console.log("Driving...");
+  }
 
-user1.greet("Hi there - I am ");
-console.log(user1);
+  loadCargo(amount: number) {
+    console.log("Loading cargo ..." + amount);
+  }
+}
+
+type Vehicle = Car | Truck; // Union type
+
+const v1 = new Car();
+const v2 = new Truck();
+
+function useVhecle(vehicles: Vehicle) {
+  vehicles.drive();
+  // if ("loadCargo" in vehicles) {
+  //   vehicles.loadCargo(1000);
+  // }
+
+  // atau bisa juga seperti ini
+
+  if (vehicles instanceof Truck) {
+    vehicles.loadCargo(1000);
+  }
+}
+
+useVhecle(v1);
+
+// discriminated unions
+interface Bird {
+  type: "bird";
+  flyingSpeed: number;
+}
+
+interface Horse {
+  type: "horse";
+  runningSpeed: number;
+}
+
+type Animal = Bird | Horse;
+
+function moveAnimal(animal: Animal) {
+  let speed;
+  switch (animal.type) {
+    case "bird":
+      speed = animal.flyingSpeed;
+      break;
+    case "horse":
+      speed = animal.runningSpeed;
+    default:
+      break;
+  }
+  console.log(" Moving at speed: " + speed);
+}
+
+moveAnimal({ type: "bird", flyingSpeed: 10 });
+
+const paragraph = document.getElementById("message-output");
+console.log(paragraph);
+
+const userInput = document.getElementById("user-input")! as HTMLInputElement; // panggunaan ! untuk meyakinkan typescript bahwa ini tidak mungkin null
+userInput.value = "This is not true";
+
+console.log(userInput);
